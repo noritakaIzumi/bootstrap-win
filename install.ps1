e228d7e6f4b80f1f77cab38c5f28a52f44074950
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-05-31 22:12:55 +0900

    refactor: iex -> Invoke-Expression

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 if (!(Get-Command choco -ErrorAction SilentlyContinue))
 {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
 ## Su admin
