998e85f22c0f941458e53e5f8b70e5befbaf07ba
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-04-16 05:05:37 +0900

    refactor: move each script installing dependencies to install.ps1

--- install.ps1
+++ install.ps1
@@ -15,6 +15,43 @@ function RunAsAdmin
     Start-Process powershell.exe "-File `"$File`" $_Args" -Verb RunAs -Wait
 }
 
+function InstallChocoDependencies
+{
+    param([string]$Config)
+
+    # Install chocolatey
+    if (!(Get-Command choco -ErrorAction SilentlyContinue))
+    {
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+
+    choco install "${Config}" -y
+}
+
+function InstallWingetDependencies
+{
+    param([string]$File)
+
+    $dependencies = [string[]](Get-Content -Path $File | Select-Object)
+    $source = "winget"
+    foreach ($dependency in $dependencies)
+    {
+        Log "Searching ${dependency} from the installed..."
+        winget list --exact --id $dependency --source $source
+        if ($?)
+        {
+            Log "Upgrading ${dependency}..."
+            winget upgrade --exact --id $dependency --source $source
+        }
+        else
+        {
+            Log "Installing ${dependency}..."
+            winget install --exact --id $dependency --source $source
+        }
+    }
+}
+
 function Main
 {
     if (-not (IsAdmin))
@@ -23,11 +60,10 @@ function Main
         exit
     }
 
-    $ScriptDir = "${PSScriptRoot}\script"
     $ConfigDir = "${PSScriptRoot}\config"
 
-    . ${ScriptDir}\choco.ps1 -Config ${ConfigDir}\choco.config
-    . ${ScriptDir}\winget.ps1 -File ${ConfigDir}\winget_dependencies.txt
+    InstallChocoDependencies -Config ${ConfigDir}\choco.config
+    InstallWingetDependencies -File ${ConfigDir}\winget_dependencies.txt
 
     # End
     Read-Host "Press enter key..."
