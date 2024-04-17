f36166556d9ae3d332189323f4df6bb590c06f1a
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-04-17 20:31:17 +0900

    update: can execute Test-Config-Chocolatey.Tests.ps1 as non-root user

--- tests/Test-Config-Chocolatey.Tests.ps1
+++ tests/Test-Config-Chocolatey.Tests.ps1
@@ -1,25 +1,12 @@
-#Requires -RunAsAdministrator
-
-$global:UseEnhancedExitCodesFeatureDisabled = (choco feature get useEnhancedExitCodes).Contains("Disabled")
-
 BeforeAll {
-    if ($UseEnhancedExitCodesFeatureDisabled)
-    {
-        choco feature enable --name="'useEnhancedExitCodes'"
-    }
 }
 
 AfterAll {
-    if ($UseEnhancedExitCodesFeatureDisabled)
-    {
-        choco feature disable --name="'useEnhancedExitCodes'"
-    }
 }
 
 Describe 'Test-Config-Chocolatey' {
     $Packages = [string[]](Get-Content -Path ${PSScriptRoot}\..\config\choco_dependencies.txt | Select-Object)
     It 'The Package "<_>" exists in chocolatey' -ForEach $Packages {
-        choco search --exact $_ > $null
-        $? | Should -Be $true
+        choco search --exact $_ | Select-String -Raw "^${_}\s" | Should -Not -BeNullOrEmpty
     }
 }
