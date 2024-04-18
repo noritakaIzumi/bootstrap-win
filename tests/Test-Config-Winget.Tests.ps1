6fe1c54c2e99c66fc3988661167a6e9905a8c10d
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-04-19 00:49:20 +0900

    test: save command output

--- tests/Test-Config-Winget.Tests.ps1
+++ tests/Test-Config-Winget.Tests.ps1
@@ -14,7 +14,7 @@ AfterAll {
 Describe 'Test-Config-Winget' {
     $Packages = [string[]](Get-Content -Path ${PSScriptRoot}\..\config\winget_dependencies.txt | Select-Object)
     It 'The Package "<_>" exists in winget' -ForEach $Packages {
-        winget search --exact --id $_ > $null
+        winget search --exact --id $_
         $? | Should -Be $true
     }
 }
