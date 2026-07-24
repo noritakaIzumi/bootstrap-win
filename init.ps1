380b0996e958917b2c863cff24d5b656a1a5ecd8
Noritaka IZUMI <noritaka.izumi@gmail.com>
2026-07-24 12:21:24 +0900

    refactor: add admin check and modularize Chocolatey installation script

--- init.ps1
+++ init.ps1
@@ -1,12 +1,59 @@
-# Install chocolatey
-if (!(Get-Command choco -ErrorAction SilentlyContinue))
+function FindPowerShellExecutable
 {
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Get-Command pwsh -ErrorAction SilentlyContinue > $null
+    if ($?)
+    {
+        "pwsh.exe"
+        return
+    }
+
+    Get-Command powershell -ErrorAction SilentlyContinue > $null
+    if ($?)
+    {
+        "powershell.exe"
+        return
+    }
+
+    Write-Error "PowerShell executable not found!!"
+    exit 1
 }
 
-# Install scoop
-if (!(Get-Command scoop -ErrorAction SilentlyContinue))
+function IsAdmin
 {
-    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    return ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")
 }
+
+function RunAsAdmin
+{
+    param([string]$File, [string]$_Args = "")
+
+    Start-Process (FindPowerShellExecutable) "-File `"$File`" $_Args" -Verb RunAs -Wait
+}
+
+function Main
+{
+    if (-not (IsAdmin))
+    {
+        RunAsAdmin -File $PSCommandPath
+        exit
+    }
+
+    # Install chocolatey
+    if (!(Get-Command choco -ErrorAction SilentlyContinue))
+    {
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+
+    # Install scoop
+    if (!(Get-Command scoop -ErrorAction SilentlyContinue))
+    {
+        Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    }
+
+    # End
+    Read-Host "Press enter key..."
+    exit
+}
+
+Main
