43428fd386e3c159da965fe7e7ce864ebe551547
Noritaka IZUMI <noritaka.izumi@gmail.com>
2026-07-24 11:33:31 +0900

    test: allow null or empty package lists in Test-Config-Chocolatey

--- tests/Test-Config-Chocolatey.Tests.ps1
+++ tests/Test-Config-Chocolatey.Tests.ps1
@@ -13,7 +13,7 @@ AfterAll {
 
 Describe 'Test-Config-Chocolatey' {
     $Packages = [string[]](Get-Content -Path ${PSScriptRoot}\..\config\choco_dependencies.txt | Select-Object)
-    It 'The Package "<_>" exists in chocolatey' -ForEach $Packages {
+    It 'The Package "<_>" exists in chocolatey' -ForEach $Packages -AllowNullOrEmptyForEach {
         choco search --exact $_ | Select-String -Raw "^${_}\s" | Should -Not -BeNullOrEmpty
     }
 }
